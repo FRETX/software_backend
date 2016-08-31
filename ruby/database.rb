911b69f31ef0b39b199312deff3fceb067eea147
Maxwell Klein <bklein@vtsmedical.com>
2016-08-30 23:59:58 -0400

    heroku pg args

--- ruby/database.rb
+++ ruby/database.rb
@@ -1,18 +1,22 @@
 def with_db
   with_catch do
     begin
-      args = {
-        :host     => '',
-        :port     => '',
-        :options  => nil,
-        :tty      => nil,
-        :dbname   => '',
-        :user     => '',
-        :password => ''
-      }
-      conn = PG::Connection.new(args)
+      conn = PG::Connection.new(get_heroku_pg_args)
       yield conn
-    ensure conn.close unless conn.nil?
-    end
+    ensure conn.close unless conn.nil? end
   end 
+end
+
+def get_heroku_pg_args
+  uri = URI.parse(ENV['DATABASE_URL'])
+  args = {
+    :host     => uri.hostname,
+    :port     => uri.port,
+    :options  => nil,
+    :tty      => nil,
+    :dbname   => uri.path[1..-1],
+    :user     => uri.user,
+    :password => uri.password
+  }
+  p args
 end

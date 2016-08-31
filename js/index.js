294de0a2b31bc7c7a9f7986b119593db9b22472f
Maxwell Klein <bklein@vtsmedical.com>
2016-08-31 02:17:50 -0400

    add link pasting

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 data = {
-  punches: [0]
+  punches: [{ "time": 0, "disp_time": '00:00:00', "chord": "No Chord" }]
+}
+
+ctrl = {
+  add_punch: function() {
+    var time = player.getCurrentTime();
+    var disp_time = secs_to_hms(time);
+    data.punches.push({ "time": time, "disp_time": disp_time, "chord": "No Chord" });
+    data.punches.sort(SortByTime);
+  },
+  delete_punch: function(e,m) {
+    var i = data.punches.indexOf(m.punch);
+  	data.punches.splice(i,1);
+  }
 }
 
 $(document).ready(function() {
@@ -9,34 +22,45 @@ $(document).ready(function() {
 });
 
 function setup_data_bindings() {
-  rivets.bind($('body'), { data: data });
+  rivets.bind($('body'), { data: data, ctrl: ctrl });
 }
 
 function setup_event_triggers() {
   $('.addvid input').on('focus', on_addvid_focus);
   $('.addvid input').on('blur', on_addvid_blur);
+  $('.addvid button').on('click', on_addvid_click);
 }
 
 function on_addvid_focus(e) {
   if(e.target.value == "Add the link to your YouTube video here..." ) { e.target.value = ''; }
+  else { e.target.select(); }
 }
 
 function on_addvid_blur(e) {
   e.target.value = e.target.value ? e.target.value : "Add the link to your YouTube video here..."
 }
 
-function popcorn() {
-  //wrapper = Popcorn.HTMLYouTubeVideoElement('#vid');
-  //wrapper.src = 'http://www.youtube.com/watch?v=CxvgCLgwdNk';
+function on_addvid_click(e) {
+  var regex = /https:\/\/www.youtube.com\/watch\?v=(.{11})/;
+  var match = regex.exec($('#link').val());
+  if(!match) { alert('invalid syntax'); return; }
+  player.loadVideoById(match[1], 0, "large")
+}
 
-  pop = Popcorn.smart('#vid', 'http://www.youtube.com/watch?v=CxvgCLgwdNk' );
+function SortByTime(a, b){ 
+  return ((a.time < b.time) ? -1 : ((a.time > b.time) ? 1 : 0));
+}
 
-  pop.footnote({
-    start: 2,
-    end: 6,
-    text: "Pop!",
-    target: "footnotediv"
-  });
+function secs_to_hms(secs) {
+  hrs  = Math.floor(secs/3600);
+  secs = secs - hrs*3600;
+  mins = Math.floor(secs/60);
+  secs = secs - mins*60;
+  secs = Math.round(secs);
 
-  pop.play();
+  hrs  = hrs.toLocaleString('en-US',  {minimumIntegerDigits: 2, useGrouping:false});
+  mins = mins.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+  secs = secs.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
+
+  return(hrs + ':' + mins + ':' + secs);
 }
--- web.rb
+++ web.rb
@@ -4,3 +4,6 @@ set :views, 'slim'
 
 Dir.chdir File.expand_path(File.dirname(__FILE__))
 Dir["ruby/*.rb"].each { |file| require File.expand_path(file); }
+
+
+# 6hrs = $300

427096415fe535a0c6132aa6ed3b2fe18765b361
Maxwell Klein <bklein@vtsmedical.com>
2016-09-14 01:17:38 -0400

    heroku script

--- ruby/environment.rb
+++ ruby/environment.rb
@@ -1,4 +1,6 @@
-`heroku config`.lines.each do |line|
-  match = /(?<key>[A-Z|_]+): +(?<value>.+)/.match(line)
-  ENV[match[:key]] = match[:value] unless match.nil?
+unless ENV.has_key?('ON_HEROKU')
+  `heroku config`.lines.each do |line|
+    match = /(?<key>[A-Z|_]+): +(?<value>.+)/.match(line)
+    ENV[match[:key]] = match[:value] unless match.nil?
+  end
 end
--- web.rb
+++ web.rb
@@ -8,4 +8,4 @@ Dir["ruby/*.rb"].each { |file| require File.expand_path(file); }
 # Wed 09/07 8pm - 3am 7hrs
 # Thu 09/08 10pm - 3am 5hrs
 # Mon 09/12 9pm - 2am 5hrs
-# Tue 09/13 
+# Tue 09/13 7pm - 1am 6hrs

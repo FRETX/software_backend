a86c8335890349a7f1c9c271dc241bff3c159526
Maxwell Klein <bklein@vtsmedical.com>
2016-12-09 01:36:23 -0500

    user to frontend

--- auth/models/User.rb
+++ auth/models/User.rb
@@ -4,11 +4,7 @@ class User < Sequel::Model
 	many_to_many :roles
 
 	def has_role?(role)
-	  p "SELF = #{self}"	
-	  p "ROLES = #{roles}"
-	  role = Role[ :name => role ]
-	  p roles.include? role
-      roles.include? role
+      roles.include? Role[:name => role]
 	end
 
 	def photo_url
